3d5392baefbf36ddfba0f85bd15d4b25e1bdf267
bharath-arcot-babu <bharath.arcotbabu@gmail.com>
2025-01-24 22:08:17 +0530

    Flask API for CRUD operations on SQL SERVER in AWS RDS

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+import os
+
+class Config:
+    SERVER = 'hexango.clqoi2aemq8p.ap-south-1.rds.amazonaws.com'
+    DATABASE = "foretale"
+    USERNAME = "admin"
+    PASSWORD = "foreHEX!2025"
+    DRIVER = "{ODBC Driver 18 for SQL Server}"
